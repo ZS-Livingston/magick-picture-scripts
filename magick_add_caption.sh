eec8418b2f8d7bf5646d24a08679b5ed3adab5e9
Albert Radvan <albert.radvan@zslivingston.cz>
2022-09-15 19:07:05 +0200

    fix: Add hashbang, channge font directory

--- magick_add_caption.sh
+++ magick_add_caption.sh
@@ -1,7 +1,14 @@
+#!/usr/bin/env bash
+
+# Displays a font warning message
+echo The script uses system installed font
+
+# Runs the caption creating loop
 for f in *.png; do
     width=$(identify -format %w "$f")
-    convert -font ~/Library/Fonts/BuenosAires-Regular.otf -background '#0008' -fill white -gravity center -size "${width}x150" caption:"${f/.png}" "$f" +swap -gravity south -composite "$f.out.png"
+    convert -font /Library/Fonts/BuenosAires-Regular.otf -background '#0008' -fill white -gravity center -size "${width}x150" caption:"${f/.png}" "$f" +swap -gravity south -composite "$f.out.png"
 done
+
 #https://legacy.imagemagick.org/Usage/annotating/
 #https://imagemagick.org/script/command-line-options.php#annotate
 #https://imagemagick.org/script/command-line-options.php#draw
